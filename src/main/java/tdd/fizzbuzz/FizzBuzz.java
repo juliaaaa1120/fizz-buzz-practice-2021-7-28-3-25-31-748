2d66e9f192fade7fcf4a353301a3acab1146d2ad
Julia Ng <ngju@oocl.com>
2021-12-02 14:07:22 +0800

    Test Case "1"

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,7 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+    public String countOff(Integer order) {
+        return order.toString();
+    }
 }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,16 @@
 package tdd.fizzbuzz;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class FizzBuzzTest {
+    private Integer order;
+
+    @Test
+    public void should_output_1_when_count_off_and_order_is_1() throws Exception {
+        order = 1;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String actualResult = fizzBuzz.countOff(order);
+        assertThat(actualResult).isEqualTo("1");
+    }
 }
