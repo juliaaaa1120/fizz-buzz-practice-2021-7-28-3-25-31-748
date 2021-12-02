109a5b3232c2c91655dba9346297720fee7f3ca4
Julia Ng <ngju@oocl.com>
2021-12-02 14:13:53 +0800

    Test Case "8"

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -61,4 +61,12 @@ public class FizzBuzzTest {
         String actualResult = fizzBuzz.countOff(order);
         assertThat(actualResult).isEqualTo("Whizz");
     }
+
+    @Test
+    public void should_output_8_when_count_off_and_order_is_8() throws Exception {
+        order = 8;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String actualResult = fizzBuzz.countOff(order);
+        assertThat(actualResult).isEqualTo("8");
+    }
 }
